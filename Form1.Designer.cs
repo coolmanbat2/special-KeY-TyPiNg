91d516e1f741572fe0f63f98871ec2cafbc88ff7
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2019-09-16 17:28:02 -0400

    specialTypeProgram

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace HelloWorldApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.customMenuOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 50);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WeRiD TyPiNg";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customMenuOptionsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(196, 26);
+            // 
+            // customMenuOptionsToolStripMenuItem
+            // 
+            this.customMenuOptionsToolStripMenuItem.Name = "customMenuOptionsToolStripMenuItem";
+            this.customMenuOptionsToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.customMenuOptionsToolStripMenuItem.Text = "Custom Menu Options";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(340, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 248);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Recipe Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem customMenuOptionsToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace HelloWorldApp
+{ 
+
+    public partial class Form1 : Form
+    {
+        const byte VK_SHIFT = 0x0010;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int PRESS_KEYDOWN = 0;
+        private const int KEYEVENTF_KEYUP = 2;
+        private const int KEYEVENTF_EXTENDEDKEY = 0XE0;
+        private static bool injectedShift = false;
+
+        // Initializers
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        
+
+        /// <summary>
+        /// main function for "weird typing"
+        /// 
+        /// </summary>
+        /// <param name="nCode"> this variable processes the message dependent on
+        /// what value it gives; i.e. nCode >= message processed; else, message not processed.</param>
+        /// <param name="wParam">the type of input that is sent to the computer. 
+        /// i.e. WM_KEYDOWN (keypressed), WM_KEYUP (key not pressed),
+        /// WM_SYSKEYDOWN (key injected press), WM_SYSKEYUP (key injected unpress)</param>
+        /// <param name="lParam"> where the type of key that is pressed is presented.</param>
+        /// <returns></returns>
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            
+            // if a key (type letter) is pressed then, 
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                // if the key pressed is a letter and not a symbol then,
+                if (vkCode >= 65 && vkCode <= 90 ||
+                    vkCode >= 97 && vkCode <= 122)
+                {
+                    // If the shift key has been pressed then, 
+                    if (Control.ModifierKeys == Keys.Shift)
+                    {
+                        // unpress the key? lol
+                        keybd_event(VK_SHIFT, 0x2a, KEYEVENTF_KEYUP, 0);
+                        injectedShift = false;
+                    }
+                    else
+                    {
+                        // otherwise, press it.
+                        keybd_event(VK_SHIFT, 0x2a, PRESS_KEYDOWN, 0);
+                        injectedShift = true;
+
+                    }
+                }
+                // otherwise if the shift key is pressed then,
+                else if (injectedShift)
+                {
+                    // needs to be one more check here. 
+                    // in which the program will "see" whether a typed character is not a non-letter.
+
+                    // needs to be something that differentiates
+                    // a normal person pressing shift versus an 
+                    // injected version of it. 
+                    // if a key is pressed then,
+                    if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+                    {
+                        // turn off the shift key so we can type normally.
+                        keybd_event(VK_SHIFT, 0xaa, KEYEVENTF_KEYUP, 0);
+                        injectedShift = false;
+                        // need some command that resets the shift key.
+                    }
+                }
+            }
+                // need to research what this does. 
+                return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            // Sets up the hook for keyboard.
+            // initilaize the keyboard map.
+            Process curProcess = Process.GetCurrentProcess();
+            ProcessModule curModule = curProcess.MainModule;
+            _hookID = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, GetModuleHandle(curModule.ModuleName), 0);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // if the shift key is pressed when closing; "unpress" it.
+            if (Control.ModifierKeys == Keys.Shift) 
+            {
+                keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+            }
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
